e584792edf93efa73154a084b7d7ecb82044eb64
Ismael Garrido Muñoz <ismaelgarridomunoz@gmail.com>
2022-07-19 10:52:03 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import CategoryToolView from "./views/CategoryToolView.vue";
 </script>
 
 <template>
@@ -15,10 +16,11 @@ import HelloWorld from "./components/HelloWorld.vue";
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <CategoryToolView></CategoryToolView>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/categorytool">Category Tool</RouterLink>
       </nav>
     </div>
   </header>
