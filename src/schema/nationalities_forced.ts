ccb4cc39612444587f91acd254a5e5c727c7e6a3
Ismael Garrido Muñoz <ismaelgarridomunoz@gmail.com>
2022-12-13 18:15:29 +0100

    asdf

---
+++ src/schema/nationalities_forced.ts
@@ -0,0 +1,15 @@
+import { words, data, models, sentences } from "@result/nationalities/FillTemplate/FillTemplate.json"
+
+import ModelResults from "@result/nationalities/EvaluateCategories/ByModel.json"
+import DimensionResults from "@result/nationalities/EvaluateCategories/ByDimension.json"
+import CategoryResults from "@result/nationalities/EvaluateCategories/ByCategory.json"
+import SentenceStatistics from "@result/nationalities/EvaluateCategories/SentenceStatistics.json"
+import SentenceDimensionStatistics from "@result/nationalities/EvaluateCategories/SentenceAndDimensionStatistics.json"
+import ModelCategoryStatistics from "@result/nationalities/EvaluateCategories/ModelCategoryStatistics.json"
+import ModelCategoryDimensionStatistics from "@result/nationalities/EvaluateCategories/ModelCategoryAndDimensionStatistics.json"
+
+const label = "Nationalities Forced";
+
+export default {
+    label, words, data, models, sentences, ModelResults, DimensionResults, CategoryResults, SentenceStatistics, SentenceDimensionStatistics, ModelCategoryStatistics, ModelCategoryDimensionStatistics
+}
